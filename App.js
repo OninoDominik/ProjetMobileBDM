81a1e1765ad226be9246cf268e26d964387227d7
Ben <benjamin.mouginot@live.fr>
2019-03-26 11:09:43 +0100

    Removing some code from App.js
    To follow the previous commit, we removed some code from our rooter 'App.js' that was used previously to navigate through the 'Page1.js' , 'Page2.js' and 'Page3.js' files.

--- App.js
+++ App.js
@@ -9,23 +9,10 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
-import Page1 from './components/Page1'
-import Page2 from './components/Page2'
-import Page3 from './components/Page3'
 import MovieScreen from './components/screens/MovieScreen'
 import MusicScreen from './components/screens/MusicScreen'
 import ShowScreen from './components/screens/ShowScreen'
 import { createMaterialTopTabNavigator, createAppContainer} from 'react-navigation'
-
-/*
-const RootStack = createStackNavigator({
-  Page1: {screen: Page1 },
-  Page2: {screen: Page2 },
-  Page3: {screen: Page3 }
-})
-
-const App = createAppContainer(RootStack);
-*/
 
 const TabNavigator = createMaterialTopTabNavigator({
   Movies: MovieScreen,
