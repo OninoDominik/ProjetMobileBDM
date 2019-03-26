71f38f122f334d4220ced1a6f8153ca92601aa70
Ben <benjamin.mouginot@live.fr>
2019-03-26 15:44:11 +0100

    Added Pulp Fiction
    Added Pulp Fiction in movie.js "database", removed some 'Avengers Infinity War' values.

--- ressources/database/movie.js
+++ ressources/database/movie.js
@@ -21,34 +21,14 @@ const movies = [
     },
     {
         key:'3', 
-        imgSrc: require('../../ressources/movieImages/AvengersInfinity.png'),
-        title: 'Avengers: Infinity War',
-        director:'Anthony & Joe Russo', 
-        date:'April 23, 2018',
-        actors: 'Robert Downey Jr, Chris Hemsworth', 
-        genre:'Superheroes, Action, Aventure',
-        synopsis:'Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.'
-    },
-    {
-        key:'4', 
-        imgSrc: require('../../ressources/movieImages/AvengersInfinity.png'),
-        title: 'Avengers: Infinity War',
-        director:'Anthony & Joe Russo', 
-        date:'April 23, 2018',
-        actors: 'Robert Downey Jr, Chris Hemsworth', 
-        genre:'Superheroes, Action, Aventure',
-        synopsis:'Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.'
-    },
-    {
-        key:'5', 
-        imgSrc: require('../../ressources/movieImages/AvengersInfinity.png'),
-        title: 'Avengers: Infinity War',
-        director:'Anthony & Joe Russo', 
-        date:'April 23, 2018',
-        actors: 'Robert Downey Jr, Chris Hemsworth', 
-        genre:'Superheroes, Action, Aventure',
-        synopsis:'Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.'
-    } 
+        imgSrc: require('../../ressources/movieImages/PulpFiction.png'),
+        title: 'Pulp Fiction',
+        director:'Quentin Tarantino', 
+        date:'October 26, 1994',
+        actors: 'Uma Thurman, John Travolta', 
+        genre:'Crime, Drama',
+        synopsis:'The lives of two mob hitmen, a boxer, a gangster\'s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.'
+    }
 ]
 
 export {movies};
